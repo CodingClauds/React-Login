10fdba2cb575e47acdf2f77a39136f1c57dcb504
Claudio Miranda <claudio_769@hotmail.com>
2022-10-19 18:32:49 -0400

    login and logout feature have been included, with the admin data it currently does function

--- src/App.js
+++ src/App.js
@@ -11,10 +11,24 @@ function App() {
   const [error, setError] = useState("");
 
   const Login = (details) => {
-    console.log(details);
+    if (
+      details.email === adminUser.email &&
+      details.password === adminUser.password
+    ) {
+      console.log("Logged In");
+
+      // Setting newState for logged in user
+      setUser({
+        name: details.name,
+        email: details.email,
+      });
+    } else {
+      console.log("Details do not match! Please confirm information");
+    }
   };
 
   const Logout = () => {
+    setUser({ name: "", email: "" });
     console.log("Logout");
   };
 
@@ -26,7 +40,7 @@ function App() {
           <h2>
             Welcome, <span>{user.name}</span>
           </h2>
-          <button>Logout</button>
+          <button onClick={Logout}>Logout</button>
         </div>
       ) : (
         <LoginForm Login={Login} error={error} />
