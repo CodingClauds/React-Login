57a6583df6117ba2119a4b03c8ae850155476494
Claudio Miranda <claudio_769@hotmail.com>
2022-11-07 12:26:03 +0000

    Remember login and forgot password markup was included in the component

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -51,11 +51,32 @@ function LoginForm({ Login, error }) {
               type="password"
               name="password"
               id="password"
+              placeholder="at least 8 characters"
               onChange={(e) =>
                 setDetails({ ...details, password: e.target.value })
               }
               value={details.password}
             />
+          </div>
+
+          <div className="remember-me">
+            <div className="remember-me__checkbox">
+              <label className="remember-me__label" for="">
+                <input
+                  className="remember-me__input"
+                  type="checkbox"
+                  name="remember"
+                  id="remember"
+                  value="yes"
+                />
+                Keep me logged in
+              </label>
+            </div>
+            <div className="forget">
+              <a className="forget__password" href="/">
+                Forgot Password?
+              </a>
+            </div>
           </div>
 
           <input className="login-btn" type="submit" value="LOGIN" />
